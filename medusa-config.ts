28c3fbd8d55812efc46e8adfe1f17f870a6e6389
djonova35 <djonova35@gmail.com>
2026-05-17 08:48:47 +0100

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -18,18 +18,25 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  admin: {
-  },
+  admin: {},
   modules: [
     {
-      resolve: "@medusajs/file-s3",
+      resolve: "@medusajs/file",
       options: {
-        file_url: process.env.S3_FILE_URL,
-        access_key_id: process.env.S3_ACCESS_KEY_ID,
-        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-        region: process.env.S3_REGION,
-        bucket: process.env.S3_BUCKET,
-        endpoint: process.env.S3_ENDPOINT,
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
       },
     },
   ],
